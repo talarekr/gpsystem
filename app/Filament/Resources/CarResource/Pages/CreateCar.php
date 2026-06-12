b25fe470afe358418aec3bc1d6300ebc766a2bd4
talarekr <talarekr@gmail.com>
2026-06-12 12:42:33 +0200

    Remove car photo UI and image handling

--- app/Filament/Resources/CarResource/Pages/CreateCar.php
+++ app/Filament/Resources/CarResource/Pages/CreateCar.php
@@ -3,30 +3,13 @@
 namespace App\Filament\Resources\CarResource\Pages;
 
 use App\Filament\Resources\CarResource;
-use App\Filament\Resources\CarResource\Pages\Concerns\ManagesCarImages;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCar extends CreateRecord
 {
-    use ManagesCarImages;
-
     protected static string $resource = CarResource::class;
 
     protected static ?string $title = 'Dodaj samochód';
-
-    /**
-     * @param array<string, mixed> $data
-     * @return array<string, mixed>
-     */
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        return $this->extractPhotoPaths($data);
-    }
-
-    protected function afterCreate(): void
-    {
-        $this->syncCarImages($this->record);
-    }
 
     protected function getCreatedNotificationTitle(): ?string
     {
