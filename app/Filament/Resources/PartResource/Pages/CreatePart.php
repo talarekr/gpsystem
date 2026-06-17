073b584b14efd0c497c917fd5444730e75c91006
talarekr <talarekr@gmail.com>
2026-06-17 12:03:49 +0200

    Simplify part photo upload form

--- app/Filament/Resources/PartResource/Pages/CreatePart.php
+++ app/Filament/Resources/PartResource/Pages/CreatePart.php
@@ -9,6 +9,21 @@ class CreatePart extends CreateRecord
 {
     protected static string $resource = PartResource::class;
 
+    protected array $partPhotoPaths = [];
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $this->partPhotoPaths = $data['part_photo_paths'] ?? [];
+        unset($data['part_photo_paths']);
+
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        PartResource::syncPartImages($this->record, $this->partPhotoPaths);
+    }
+
     protected function getCreateFormAction(): \Filament\Actions\Action
     {
         return parent::getCreateFormAction()->label('Dodaj część');
