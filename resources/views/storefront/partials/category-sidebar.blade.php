7e1439679223d64216ff21892f7ce52b1ef0f428
talarekr <talarekr@gmail.com>
2026-06-19 13:27:23 +0200

    Refine storefront category sidebar

--- resources/views/storefront/partials/category-sidebar.blade.php
+++ resources/views/storefront/partials/category-sidebar.blade.php
@@ -2,16 +2,38 @@
     $categoryTreeService ??= app(\App\Services\Storefront\CategoryTreeService::class);
     $categoryRoots ??= $categoryTreeService->roots();
     $activeCategory ??= null;
-    $activeAncestorIds = $activeCategory ? $categoryTreeService->ancestors($activeCategory)->pluck('id')->all() : [];
+    $activeAncestorIds = [];
+
+    if ($activeCategory) {
+        $activeChildren = $activeCategory->children ?? collect();
+        $allCategories = $categoryTreeService->all();
+        $parentCategory = $activeCategory->parent_id ? $allCategories->get($activeCategory->parent_id) : null;
+
+        $sidebarCategories = $activeChildren->isNotEmpty()
+            ? $activeChildren
+            : ($parentCategory?->children ?? collect([$activeCategory]));
+    } else {
+        $sidebarCategories = $categoryRoots;
+    }
 @endphp
 
 <aside class="sf-category-sidebar">
-    <h3>Kategorie części</h3>
+    <h3>{{ $activeCategory ? 'Kategorie w tej sekcji' : 'Kategorie części' }}</h3>
+
+    @if($activeCategory)
+        <a class="sf-category-sidebar__current" href="{{ $categoryTreeService->url($activeCategory) }}">
+            <span>{{ $activeCategory->name }}</span>
+            @if(! is_null($activeCategory->woo_product_count))
+                <small>{{ $activeCategory->woo_product_count }}</small>
+            @endif
+        </a>
+    @endif
+
     @include('storefront.partials.category-tree', [
-        'categories' => $categoryRoots,
+        'categories' => $sidebarCategories,
         'activeCategory' => $activeCategory,
         'activeAncestorIds' => $activeAncestorIds,
         'level' => 0,
-        'maxDepth' => 2,
+        'maxDepth' => 0,
     ])
 </aside>
