a88d9af99dedf2ee459bed1a1cf1277041d7cab6
talarekr <talarekr@gmail.com>
2026-06-19 22:13:19 +0200

    Fix storefront catalog Blade syntax

--- resources/views/storefront/partials/header.blade.php
+++ resources/views/storefront/partials/header.blade.php
@@ -1,8 +1,8 @@
 <header class="sf-top">
     <div class="sf-container sf-top__inner">
         <div class="sf-top__links">
-            <a href="{{ route('storefront.contact') }}">Kontakt</a>
-            <a href="{{ route('storefront.terms') }}">Regulamin</a>
+            <a href="<?= e(route('storefront.contact')) ?>">Kontakt</a>
+            <a href="<?= e(route('storefront.terms')) ?>">Regulamin</a>
         </div>
         <label class="sf-language" aria-label="Wybór języka strony">
             <select class="sf-language-select" name="language">
@@ -16,51 +16,51 @@
     </div>
 </header>
 <div class="sf-container sf-main-row">
-    @php
+    <?php
         $storefrontLogoPath = '/storage/brand/logo.png';
         $storefrontLogoPublicHtmlPath = dirname(base_path()) . '/public_html/storage/brand/logo.png';
         $storefrontLogoExists = file_exists($storefrontLogoPublicHtmlPath) || file_exists(public_path('storage/brand/logo.png'));
-    @endphp
-    <a class="sf-logo" href="{{ route('storefront.home') }}" aria-label="GP Swiss - strona główna">
-        @if($storefrontLogoExists)
-            <img src="{{ $storefrontLogoPath }}" alt="GP Swiss" style="display:block;height:clamp(38px,8vw,50px);width:auto;max-width:100%;object-fit:contain;">
-        @else
+    ?>
+    <a class="sf-logo" href="<?= e(route('storefront.home')) ?>" aria-label="GP Swiss - strona główna">
+        <?php if ($storefrontLogoExists): ?>
+            <img src="<?= e($storefrontLogoPath) ?>" alt="GP Swiss" style="display:block;height:clamp(38px,8vw,50px);width:auto;max-width:100%;object-fit:contain;">
+        <?php else: ?>
             <span>GP</span>Swiss
-        @endif
+        <?php endif; ?>
     </a>
-    @include('storefront.partials.search-bar')
+    <?php echo $__env->make('storefront.partials.search-bar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
     <details class="sf-profile">
         <summary><span aria-hidden="true">👤</span> Moje konto</summary>
         <div>
             <?php $storefrontCustomer = auth()->user(); ?>
             <?php if ($storefrontCustomer): ?>
-                <a href="{{ route('storefront.account') }}">Panel klienta</a>
-                <a href="{{ route('storefront.account') }}#orders">Zamówienia</a>
-                <form method="post" action="{{ route('storefront.logout') }}">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <a href="<?= e(route('storefront.account')) ?>">Panel klienta</a>
+                <a href="<?= e(route('storefront.account')) ?>#orders">Zamówienia</a>
+                <form method="post" action="<?= e(route('storefront.logout')) ?>">
+                    <input type="hidden" name="_token" value="<?= e(csrf_token()) ?>">
                     <button type="submit">Wyloguj się</button>
                 </form>
             <?php else: ?>
-                <a href="{{ route('storefront.login') }}">Zaloguj się</a>
-                <a href="{{ route('storefront.register') }}">Zarejestruj się</a>
+                <a href="<?= e(route('storefront.login')) ?>">Zaloguj się</a>
+                <a href="<?= e(route('storefront.register')) ?>">Zarejestruj się</a>
             <?php endif; ?>
         </div>
     </details>
-    <a class="sf-cart sf-cart--icon" href="{{ route('storefront.cart.index') }}" aria-label="Koszyk"><span aria-hidden="true">🛒</span><b>{{ $storefrontCartCount ?? 0 }}</b></a>
+    <a class="sf-cart sf-cart--icon" href="<?= e(route('storefront.cart.index')) ?>" aria-label="Koszyk"><span aria-hidden="true">🛒</span><b><?= e($storefrontCartCount ?? 0) ?></b></a>
 </div>
-@php
+<?php
     $storefrontMainLinks = [
         'Silniki' => '/kategoria-produktu/silnik-i-osprzet/silniki-i-osprzet/kompletne-silniki',
         'Skrzynia biegów' => '/kategoria-produktu/uklad-napedowy/skrzynie-biegow-i-inne-elementy/automatyczna-skrzynia-biegow',
         'Filtry DPF' => '/kategoria-produktu/uklad-wydechowy-i-inne-elementy/elementy-systemu-kontroli-spalin/filtr-czastek-stalych-katalizator-fap-dpf',
         'Zwrotnice' => '/kategoria-produktu/os-przednia-i-inne-elementy/os-przednia/zwrotnica-kola-przedniego',
     ];
-@endphp
+?>
 <nav class="sf-nav"><div class="sf-container sf-nav__inner">
-    @include('storefront.partials.category-menu')
+    <?php echo $__env->make('storefront.partials.category-menu', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
     <div class="sf-nav__links" aria-label="Menu główne">
         <?php foreach ($storefrontMainLinks as $label => $url): ?>
-            <a href="{{ $url }}">{{ $label }}</a>
+            <a href="<?= e($url) ?>"><?= e($label) ?></a>
         <?php endforeach; ?>
     </div>
     <span class="sf-phones">📞 <a href="tel:+48504266984">+48 504 266 984</a>&nbsp;&nbsp; <a href="tel:+48579152665">+48 579 152 665</a></span>
