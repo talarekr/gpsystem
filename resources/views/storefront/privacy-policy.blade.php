d326b8dfe5df722e68a7e8e93926c186b85ef855
talarekr <talarekr@gmail.com>
2026-06-19 16:06:25 +0200

    Update storefront menu and privacy page

---
+++ resources/views/storefront/privacy-policy.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.storefront')
+
+@section('content')
+<div class="sf-container sf-page sf-static-page">
+    @include('storefront.partials.breadcrumbs')
+
+    <article class="sf-static-card sf-terms">
+        <h1>Polityka prywatności</h1>
+
+        <h2>1. Administrator danych</h2>
+        <p>Administratorem danych osobowych jest:</p>
+        <p>GREGOR swiss GRZEGORZ PACIOREK<br>ul. Milanowska 137<br>08-460 Sobolew<br>NIP: 8262157853<br>REGON: 368948917</p>
+        <p>(dalej: „Administrator”)</p>
+        <p>W sprawach związanych z przetwarzaniem danych osobowych można kontaktować się pod adresem:<br><a href="mailto:biuro@gpswiss.pl">biuro@gpswiss.pl</a></p>
+
+        <h2>2. Informacje ogólne</h2>
+        <p>Niniejsza Polityka Prywatności określa zasady przetwarzania danych osobowych oraz wykorzystywania plików cookies w związku z korzystaniem ze strony internetowej <a href="https://gpswiss.pl/">https://gpswiss.pl/</a>.</p>
+
+        <h2>3. Zakres przetwarzanych danych</h2>
+        <p>W zależności od sposobu korzystania z serwisu przetwarzane mogą być następujące dane:</p>
+        <ul>
+            <li>imię i nazwisko,</li>
+            <li>adres e-mail,</li>
+            <li>numer telefonu,</li>
+            <li>adres dostawy,</li>
+            <li>dane do faktury,</li>
+            <li>adres IP,</li>
+            <li>dane dotyczące zamówień,</li>
+            <li>dane konta użytkownika,</li>
+            <li>dane pozyskane z logowania przez Google (jeśli użytkownik skorzysta z tej opcji).</li>
+        </ul>
+
+        <h2>4. Cele przetwarzania danych</h2>
+        <p>Dane osobowe przetwarzane są w celu:</p>
+        <ul>
+            <li>realizacji zamówień i obsługi klienta,</li>
+            <li>prowadzenia konta użytkownika,</li>
+            <li>obsługi płatności,</li>
+            <li>kontaktu z użytkownikiem,</li>
+            <li>realizacji obowiązków prawnych (np. podatkowych),</li>
+            <li>zapewnienia bezpieczeństwa serwisu,</li>
+            <li>prowadzenia statystyk,</li>
+            <li>integracji z platformą Allegro (w zakresie realizacji sprzedaży),</li>
+            <li>umożliwienia logowania przez Google (OAuth).</li>
+        </ul>
+
+        <h2>5. Logowanie przez Google</h2>
+        <p>Serwis umożliwia logowanie oraz rejestrację za pomocą konta Google.</p>
+        <p>W przypadku skorzystania z tej funkcji mogą być pobierane następujące dane:</p>
+        <ul>
+            <li>adres e-mail,</li>
+            <li>identyfikator konta Google (sub),</li>
+            <li>imię i nazwisko (jeśli dostępne),</li>
+            <li>zdjęcie profilowe (opcjonalnie),</li>
+            <li>informacja o weryfikacji adresu e-mail.</li>
+        </ul>
+        <p>Dane te są wykorzystywane wyłącznie w celu:</p>
+        <ul>
+            <li>utworzenia konta użytkownika,</li>
+            <li>logowania użytkownika,</li>
+            <li>powiązania konta Google z kontem w serwisie.</li>
+        </ul>
+
+        <h2>6. Udostępnianie danych</h2>
+        <p>Dane mogą być przekazywane podmiotom trzecim wyłącznie w zakresie niezbędnym do realizacji usług:</p>
+        <ul>
+            <li>operatorom płatności,</li>
+            <li>firmom kurierskim i logistycznym,</li>
+            <li>dostawcom usług hostingowych,</li>
+            <li>dostawcom narzędzi analitycznych,</li>
+            <li>platformie Allegro (w zakresie realizacji zamówień),</li>
+            <li>Google (w przypadku korzystania z logowania Google).</li>
+        </ul>
+
+        <h2>7. Pliki cookies</h2>
+        <p>Strona wykorzystuje pliki cookies w celu:</p>
+        <ul>
+            <li>prawidłowego działania serwisu,</li>
+            <li>utrzymania sesji użytkownika,</li>
+            <li>obsługi logowania,</li>
+            <li>zapamiętywania preferencji,</li>
+            <li>analizy ruchu na stronie.</li>
+        </ul>
+        <p>Użytkownik może w każdej chwili zmienić ustawienia cookies w swojej przeglądarce.</p>
+
+        <h2>8. Okres przechowywania danych</h2>
+        <p>Dane osobowe przechowywane są przez okres:</p>
+        <ul>
+            <li>trwania umowy (konto użytkownika),</li>
+            <li>wymagany przepisami prawa (np. księgowość),</li>
+            <li>do momentu wycofania zgody (jeśli dotyczy),</li>
+            <li>niezbędny do realizacji celów przetwarzania.</li>
+        </ul>
+
+        <h2>9. Prawa użytkownika</h2>
+        <p>Użytkownik ma prawo do:</p>
+        <ul>
+            <li>dostępu do swoich danych,</li>
+            <li>ich sprostowania,</li>
+            <li>usunięcia („prawo do bycia zapomnianym”),</li>
+            <li>ograniczenia przetwarzania,</li>
+            <li>przenoszenia danych,</li>
+            <li>wniesienia sprzeciwu,</li>
+            <li>złożenia skargi do Prezesa Urzędu Ochrony Danych Osobowych (UODO).</li>
+        </ul>
+
+        <h2>10. Zabezpieczenia danych</h2>
+        <p>Administrator stosuje odpowiednie środki techniczne i organizacyjne w celu ochrony danych osobowych przed ich utratą, nieuprawnionym dostępem lub ujawnieniem.</p>
+
+        <h2>11. Zmiany polityki prywatności</h2>
+        <p>Polityka prywatności może być aktualizowana. Nowa wersja będzie publikowana na tej stronie.</p>
+
+        <h2>12. Postanowienia końcowe</h2>
+        <p>Korzystanie z serwisu oznacza akceptację niniejszej Polityki Prywatności.</p>
+    </article>
+</div>
+@endsection
