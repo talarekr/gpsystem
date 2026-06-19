e8e53a115990d95ba590b0924e0d6a6367d438cc
talarekr <talarekr@gmail.com>
2026-06-19 15:49:18 +0200

    Update storefront header and hero copy

--- resources/views/storefront/partials/header.blade.php
+++ resources/views/storefront/partials/header.blade.php
@@ -6,15 +6,31 @@
         }
     }
     .sf-top .sf-top__inner {
-        justify-content: center;
+        justify-content: space-between;
         flex-wrap: wrap;
         overflow-x: visible;
         white-space: normal;
         text-align: center;
     }
-    .sf-top__highlight {
+    .sf-top__links {
+        display: inline-flex;
+        gap: 22px;
+        align-items: center;
+    }
+    .sf-language-select {
+        border: 1px solid var(--line);
+        border-radius: 8px;
+        background: #fff;
         color: var(--navy);
+        padding: 5px 9px;
+        font: inherit;
         font-weight: 700;
+    }
+    .sf-nav__links {
+        display: flex;
+        justify-content: center;
+        gap: 24px;
+        flex: 1 1 auto;
     }
     @media (min-width: 768px) {
         .sf-top .sf-top__inner {
@@ -23,7 +39,23 @@
         }
     }
 </style>
-<header class="sf-top"><div class="sf-container sf-top__inner"><a href="{{ route('storefront.contact') }}">Kontakt</a><span class="sf-top__highlight">90 dni gwarancji na Silniki, skrzynie biegów i dyferencjały</span><a href="{{ route('storefront.terms') }}">Regulamin</a></div></header>
+<header class="sf-top">
+    <div class="sf-container sf-top__inner">
+        <div class="sf-top__links">
+            <a href="{{ route('storefront.contact') }}">Kontakt</a>
+            <a href="{{ route('storefront.terms') }}">Regulamin</a>
+        </div>
+        <label class="sf-language" aria-label="Wybór języka strony">
+            <select class="sf-language-select" name="language">
+                <option selected>Polski</option>
+                <option>Angielski</option>
+                <option>Niemiecki</option>
+                <option>Francuski</option>
+                <option>Ukraiński</option>
+            </select>
+        </label>
+    </div>
+</header>
 <div class="sf-container sf-main-row">
     @php($storefrontLogoPath = '/storage/brand/logo.png')
     @php($storefrontLogoPublicHtmlPath = dirname(base_path()) . '/public_html/storage/brand/logo.png')
@@ -39,10 +71,20 @@
     <details class="sf-profile"><summary>Moje konto</summary><div><a href="#">Logowanie</a><a href="#">Zamówienia</a></div></details>
     <a class="sf-cart" href="{{ route('storefront.cart.index') }}">Koszyk <b>{{ $storefrontCartCount ?? 0 }}</b></a>
 </div>
+@php
+    $storefrontMainLinks = [
+        'Silniki' => '/kategoria-produktu/silnik-i-osprzet/silniki-i-osprzet/kompletne-silniki',
+        'Skrzynia biegów' => '/kategoria-produktu/uklad-klimatyzacji-wentylacji-chlodzenia-silnika-ogrzewanie-postojowe/chlodnice-i-wentylatory/chlodnica-oleju-skrzynia-biegow',
+        'Filtry DPF' => '/kategoria-produktu/uklad-wydechowy-i-inne-elementy/elementy-systemu-kontroli-spalin/filtr-czastek-stalych-katalizator-fap-dpf',
+        'Zwrotnice' => '/kategoria-produktu/os-przednia-i-inne-elementy/os-przednia/zwrotnica-kola-przedniego',
+    ];
+@endphp
 <nav class="sf-nav"><div class="sf-container sf-nav__inner">
     @include('storefront.partials.category-menu')
-    @foreach($storefrontCategoryShortcuts ?? [] as $shortcut)
-        <a href="{{ $shortcut['url'] }}">{{ $shortcut['label'] }}</a>
-    @endforeach
+    <div class="sf-nav__links" aria-label="Menu główne">
+        @foreach($storefrontMainLinks as $label => $url)
+            <a href="{{ $url }}">{{ $label }}</a>
+        @endforeach
+    </div>
     <span class="sf-phones">📞 +48 504 266 984&nbsp;&nbsp; +48 579 152 665</span>
 </div></nav>
