797049a056fa0231e12d19fd5f342f86d583cf48
talarekr <talarekr@gmail.com>
2026-06-27 12:18:58 +0200

    Revert "Topbar admina: usuń czarne pasy logo i wyrównaj przycisk Zamówienia"

--- resources/views/filament/admin-topbar.blade.php
+++ resources/views/filament/admin-topbar.blade.php
@@ -20,34 +20,32 @@
 @endphp
 
 <div class="gps-admin-topbar" data-gps-admin-topbar>
-    <div class="gps-admin-topbar__inner">
-        <div class="gps-admin-topbar__left">
-            <button class="gps-admin-topbar__hamburger" type="button" data-gps-sidebar-toggle aria-label="Zwiń lub rozwiń menu boczne" aria-expanded="true">
-                <span></span><span></span><span></span>
-            </button>
+    <div class="gps-admin-topbar__left">
+        <button class="gps-admin-topbar__hamburger" type="button" data-gps-sidebar-toggle aria-label="Zwiń lub rozwiń menu boczne" aria-expanded="true">
+            <span></span><span></span><span></span>
+        </button>
 
-            <a class="gps-admin-topbar__brand" href="{{ url('/admin') }}" aria-label="GP Swiss">
-                <span class="gps-admin-topbar__brand-mark">
-                    <img src="{{ $adminLogoUrl }}" alt="" loading="eager">
-                </span>
-                <span class="gps-admin-topbar__brand-name">GP Swiss</span>
-            </a>
+        <a class="gps-admin-topbar__brand" href="{{ url('/admin') }}" aria-label="GP Swiss">
+            <span class="gps-admin-topbar__brand-mark">
+                <img src="{{ $adminLogoUrl }}" alt="" loading="eager">
+            </span>
+            <span class="gps-admin-topbar__brand-name">GP Swiss</span>
+        </a>
+    </div>
+
+    <div class="gps-admin-topbar__center">
+        <div class="gps-admin-search" data-gps-part-search data-endpoint="{{ route('admin.search.parts') }}">
+            <span class="gps-admin-search__icon" aria-hidden="true">⌕</span>
+            <input class="gps-admin-search__input" type="search" placeholder="Szukaj części: SKU, OEM, numer, nazwa…" autocomplete="off" data-gps-part-search-input>
+            <div class="gps-admin-search__dropdown" data-gps-part-search-results hidden></div>
         </div>
 
-        <div class="gps-admin-topbar__center">
-            <div class="gps-admin-search" data-gps-part-search data-endpoint="{{ route('admin.search.parts') }}">
-                <span class="gps-admin-search__icon" aria-hidden="true">⌕</span>
-                <input class="gps-admin-search__input" type="search" placeholder="Szukaj części: SKU, OEM, numer, nazwa…" autocomplete="off" data-gps-part-search-input>
-                <div class="gps-admin-search__dropdown" data-gps-part-search-results hidden></div>
-            </div>
+        <a class="gps-admin-topbar__parts-link" href="{{ $partsIndexUrl }}">Wszystkie części</a>
+    </div>
 
-            <a class="gps-admin-topbar__parts-link" href="{{ $partsIndexUrl }}">Wszystkie części</a>
-        </div>
-
-        <div class="gps-admin-topbar__right">
-            <a class="gps-admin-topbar__action" href="{{ $ordersUrl }}" title="Zamówienia">
-                <span aria-hidden="true">📋</span><span>Zamówienia</span><strong>{{ $ordersCount }}</strong>
-            </a>
-        </div>
+    <div class="gps-admin-topbar__right">
+        <a class="gps-admin-topbar__action" href="{{ $ordersUrl }}" title="Zamówienia">
+            <span aria-hidden="true">📋</span><span>Zamówienia</span><strong>{{ $ordersCount }}</strong>
+        </a>
     </div>
 </div>
