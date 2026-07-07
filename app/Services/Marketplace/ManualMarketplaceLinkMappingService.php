db732d0fb3973257e2de30d7f8b353f274c08d25
talarekr <talarekr@gmail.com>
2026-07-07 20:01:30 +0200

    Revert "Allow idempotent Ovoko manual link saves"

--- app/Services/Marketplace/ManualMarketplaceLinkMappingService.php
+++ app/Services/Marketplace/ManualMarketplaceLinkMappingService.php
@@ -41,20 +41,12 @@ class ManualMarketplaceLinkMappingService
             throw new InvalidArgumentException('Nie udało się odczytać ID oferty z linku '.ucfirst($marketplace).'. Sprawdź, czy wklejony adres prowadzi do konkretnej oferty.');
         }
 
-        $listings = MarketplaceListing::query()
+        $listing = MarketplaceListing::query()
             ->where('part_id', $part->getKey())
             ->whereIn('marketplace', $marketplace === 'allegro' ? ['allegro', 'allegro_main'] : ['ovoko'])
             ->orderByDesc('id')
-            ->get();
+            ->first();
 
-        $matchingListing = $listings->first(fn (MarketplaceListing $listing): bool => $this->listingExternalId($listing) === $externalId);
-        if ($matchingListing) {
-            $matchingListing->forceFill($this->attributes($part, $marketplace, $externalId, $url, $matchingListing->raw_payload ?? []))->save();
-
-            return $this->result($matchingListing, $marketplace, $externalId, $url, 'updated');
-        }
-
-        $listing = $listings->first();
         if ($listing) {
             $existingId = $this->listingExternalId($listing);
 
